a537c545c2ed1dec222b443b5f4290073686cb3e
Drew Overcash <drewovercash@gmail.com>
2017-11-05 16:33:31 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -2,6 +2,8 @@ function forLoop (array) {
   for (var i = 1; i < 25; i++) {
     if (i > 1) {
       console.log(`I am ${i} strange loops.`);
-    } console.log(`I am ${i} strange loop.`);
+    } else {
+      console.log(`I am ${i} strange loop.`);
+};
 };
 };
